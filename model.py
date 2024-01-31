e2ab58a0b2c634240200d8ad1e465e95577be091
Dharmendra Yeole <126152052+dharmendrayeole@users.noreply.github.com>
2024-01-31 15:38:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 # Importing libraries-----------------------------------------------------------------------------------------
 import streamlit as st
 import pandas as pd
